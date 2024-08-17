a15d1cb71688f04ece14f8b40eaa9ddeffcaac5e
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-08-17 23:45:13 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import blogData from "../data/blog";
+import Article from "./Article";
+import Header from "./Header";
+import About from "./About";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
+      <Header name={blogData.name} />
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      <About />
+      <Article />
     </div>
   );
 }
